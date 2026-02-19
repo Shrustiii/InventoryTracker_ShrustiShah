5f22042eafd106d2669722fc19fbca5e8fdf0669
Shrusti Shah <shshrush@sheridancollege.ca>
2026-02-19 17:06:10 -0500

    Project steup by Shrusti Shah

---
+++ src/main/java/shshrush/inventorytracker/InventoryTracker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package shshrush.inventorytracker;
+
+/**
+ *
+ * @author shrustishah
+ */
+public class InventoryTracker {
+
+    public static void main(String[] args) {
+        System.out.println("Inventory Tracker!");
+    }
+}
